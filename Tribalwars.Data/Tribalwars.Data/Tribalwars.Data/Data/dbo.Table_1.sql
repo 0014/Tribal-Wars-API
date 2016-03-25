603d0a8b5b2acc80398e7191051044aac1ef31dd
0014 <a.gencosmanoglu@gmail.com>
2016-03-25 10:51:54 -0400

    Removed data

--- Tribalwars.Data/Tribalwars.Data/Tribalwars.Data/Data/dbo.Table_1.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[BuildingDetails] (
-    [Name]               INT      NOT NULL,
-    [ConstructionTime] TIME (7) NOT NULL,
-    [Level]            INT      NOT NULL,
-    [Wood]             INT      NOT NULL,
-    [Clay]             INT      NOT NULL,
-    [Iron]             INT      NOT NULL,
-);
