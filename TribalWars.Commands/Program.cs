86742f0b8c6a39e6d5803e76e39d3e95332c0fdb
0014 <a.gencosmanoglu@gmail.com>
2016-04-20 18:14:57 -0400

    Batch files now work
    Only attacking command is added. Building command will be added soon.

--- TribalWars.Commands/Program.cs
+++ TribalWars.Commands/Program.cs
@@ -10,29 +10,31 @@ namespace TribalWars.Commands
 
         static void Main(string[] args)
         {
-            string[] array = new string[14] { "Attack", "21654", "509", "355", "1", "0", "0", "0", "0", "0", "0", "0", "0", "0" };
+            string[] array = new string[13] { "Attack", "509", "355", "1", "0", "0", "0", "0", "0", "0", "0", "0", "0" };
 
-            Attack(array);
-            /*if (args[0].Equals("Attack"))
-                Attack(args);
+            var command = new LoginActions();
+            var token = command.Login();
+            
+            if (args[0].Equals("Attack"))
+                Attack(token, args);
             else if (args[0].Equals("Attack")) 
                 Build(args);
             else
-                Console.WriteLine("Wrong command entered.");*/
+                Console.WriteLine("Wrong command entered.");
         }
 
-        private static void Attack(string[] args)
+        private static void Attack(string token, string[] args)
         {
             var armyInfo = new int[10];
 
-            var command = new FarmActions(args[1]);
+            var command = new FarmActions(token);
 
             var coordinates = new int[2];
-            coordinates[X] = int.Parse(args[2]);
-            coordinates[Y] = int.Parse(args[3]);
+            coordinates[X] = int.Parse(args[1]);
+            coordinates[Y] = int.Parse(args[2]);
 
-            for (var i = 4; i < args.Length; i++)
-                armyInfo[i - 4] = int.Parse(args[i]);
+            for (var i = 3; i < args.Length; i++)
+                armyInfo[i - 3] = int.Parse(args[i]);
             
             var army = new ArmyBuilder(armyInfo);
 
